92acb32c926450575d7915771fb4d5b00790e2e4
zendrias <zenas@live.com>
2022-03-23 19:43:59 -0400

    create make lobby route

---
+++ src/pages/AddAGame/AddAGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddAGame(props) {
+  return (
+    <>
+      <h1>Add A Game</h1>
+    </>
+  );
+}
+
+export default AddAGame;
---
+++ src/pages/MakeALobby/MakeALobby.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MakeALobby(props) {
+  return (
+    <>
+      <h1>Make A Lobby</h1>
+    </>
+  );
+}
+
+export default MakeALobby;
