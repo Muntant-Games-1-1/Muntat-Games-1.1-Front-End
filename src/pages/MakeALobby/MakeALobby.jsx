5495071650af7bd8bf6a14f9bde32fd82c0335d8
zendrias <zenas@live.com>
2022-03-23 20:11:40 -0400

    create add lobby form

--- src/pages/MakeALobby/MakeALobby.jsx
+++ src/pages/MakeALobby/MakeALobby.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 function MakeALobby(props) {
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
     <>
-      <h1>Make A Lobby</h1>
+      <h1>Create A Lobby</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="lobby-name">Lobby Name</label>
+        <input type="text" id='lobby-name' />
+        <label htmlFor="lobby-name">Choose A Game</label>
+        <select id='lobby-name'>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+        </select>
+        <label htmlFor="lobby-limit">Player Limit</label>
+        <input type="text" id='lobby-limit' />
+        <label htmlFor="player-rank">Minimum Rank</label>
+        <select id='player-rank'>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+        </select>
+        <button type='submit'>Create</button>
+      </form>
     </>
   );
 }
