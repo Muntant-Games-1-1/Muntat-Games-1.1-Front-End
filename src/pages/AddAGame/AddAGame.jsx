853b187d543221d456572133368972b125ae8a20
zendrias <zenas@live.com>
2022-03-25 13:21:53 -0400

    stub up AddAGame form

--- src/pages/AddAGame/AddAGame.jsx
+++ src/pages/AddAGame/AddAGame.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function AddAGame(props) {
+
+  const [validForm, setValidForm] = useState(false)
+
+  const formElement = useRef()
+  // ! Make Sure To Set The Drop Down Menu Inputs Initial State To The First Selection
+  const [formData, setFormData] = useState({
+    lobbyName: '',
+    // chooseGame: '',
+    // lobbyLimit: '',
+    // playerRank: ''
+  })
+
+  // Action Handlers
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    handleCreateLobby(formData)
+  }
+
+  // Side-Effects
+  useEffect(() => {
+    formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
+  }, [formData])
+
   return (
     <>
-      <h1>Add A Game</h1>
+      <h1>Create A Lobby</h1>
+      <form onSubmit={handleSubmit} ref={formElement}>
+        <label htmlFor="lobbyName">Lobby Name</label>
+        <input
+          required
+          type="text"
+          id='lobbyName'
+          name='name'
+          onChange={handleChange}
+        />
+        <label htmlFor="chooseGame">Choose A Game</label>
+        <select id='choose-game' onChange={handleChange} name='chooseGame' value={formData.chooseGame}>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+        </select>
+        <label htmlFor="lobbyLimit">Player Limit</label>
+        <input
+          type="text"
+          id='lobbyLimit'
+          name='lobbyLimit'
+          onChange={handleChange} />
+        <label htmlFor="playerRank">Minimum Rank</label>
+        <select id='playerRank' name='playerRank' onChange={handleChange}>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+        </select>
+        <button type='submit' disabled={!validForm}>Create</button>
+      </form>
     </>
   );
 }
-
 export default AddAGame;
+
