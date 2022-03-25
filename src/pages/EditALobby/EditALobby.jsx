8bd1a45074eba27351eaccec5a8c0bd4048737f8
zendrias <zenas@live.com>
2022-03-25 11:33:03 -0400

    copy paste add lobby form

--- src/pages/EditALobby/EditALobby.jsx
+++ src/pages/EditALobby/EditALobby.jsx
@@ -2,8 +2,35 @@ import React from 'react';
 
 function EditALobby(props) {
   return (
-    <div>
+    <div className='lobby-form'>
       <h1>Edit A Lobby</h1>
+      <form onSubmit={handleSubmit} ref={formElement}>
+        <label htmlFor="lobbyName">Lobby Name</label>
+        <input
+          required
+          type="text"
+          id='lobbyName'
+          name='name'
+          onChange={handleChange}
+        />
+        <label htmlFor="chooseGame">Choose A Game</label>
+        <select id='choose-game' onChange={handleChange} name='chooseGame' value={formData.chooseGame}>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+        </select>
+        <label htmlFor="lobbyLimit">Player Limit</label>
+        <input
+          type="text"
+          id='lobbyLimit'
+          name='lobbyLimit'
+          onChange={handleChange} />
+        <label htmlFor="playerRank">Minimum Rank</label>
+        <select id='playerRank' name='playerRank' onChange={handleChange}>
+          <option value="sample">sample</option>
+          <option value="sample">sample</option>
+        </select>
+        <button type='submit' disabled={!validForm}>Create</button>
+      </form>
     </div>
   );
 }
