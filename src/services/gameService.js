eba99c513273766919c2d50aa18d8106bdd5308f
zendrias <zenas@live.com>
2022-03-25 16:03:11 -0400

    write addGame service function

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -10,11 +10,14 @@ export async function getCategories(){
   return categories.json()
 }
 
-export async function addGame(){
+export async function addGame(gameDetails){
   const game = await fetch(`${BASE_URL}`, {
     method: 'POST', 
     headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`
-    }
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(gameDetails)
   })
+  return game.json()
 }
