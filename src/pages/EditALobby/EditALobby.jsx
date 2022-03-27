4150af6ae7f8d37c53f5bfe4d1ca340c9e5507d3
zendrias <zenas@live.com>
2022-03-27 17:07:29 -0400

    progress debugging

--- src/pages/EditALobby/EditALobby.jsx
+++ src/pages/EditALobby/EditALobby.jsx
@@ -4,10 +4,10 @@ import {useLocation} from 'react-router-dom'
 // ! Change All The Input Values To Have The Pre-Edit Data
 function EditALobby({games, handleEditLobby}) {
   const location = useLocation()
-  console.log(location.state)
-  const previousGame =  games?.filter(gs =>{
-    return gs._id.toString() === location?.state?.game.toString()
-  })
+  // console.log(location.state)
+  // const previousGame =  games?.filter(gs =>{
+  //   return gs._id.toString() === location?.state?.game.toString()
+  // })
   const [validForm, setValidForm] =useState(true)
   const formElement = useRef()
   const [formData, setFormData] = useState(location.state)
@@ -37,9 +37,9 @@ useEffect(() =>{
           value={formData.name}
         />
         <label htmlFor="chooseGame">Choose A Game</label>
-        <select id='chooseGame' onChange={handleChange} name='game' value={formData?.chooseGame}>
+        <select id='chooseGame' onChange={handleChange} name='game' value={formData.chooseGame}>
           {games && games.map((game, i) =>{
-            return <option value={i}>hello</option>
+            return <option value={i}>{game.name}</option>
           })}
         </select>
         <label htmlFor="lobbyLimit">Player Limit</label>
