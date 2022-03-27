3c213bc49a318a9941264818621448a32621a406
zendrias <zenas@live.com>
2022-03-27 18:07:32 -0400

    remove console.log's

--- src/pages/MakeALobby/MakeALobby.jsx
+++ src/pages/MakeALobby/MakeALobby.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 
 function MakeALobby({ handleCreateLobby, games }) {
-console.log(games)
   const [validForm, setValidForm] = useState(false)
 
   const formElement = useRef()
