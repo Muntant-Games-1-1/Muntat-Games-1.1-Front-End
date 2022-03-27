f76e8cebaa273295529c733ca220a8653090bd7b
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-27 17:22:24 -0400

    adding css to alobby

--- src/pages/LobbyDetail/LobbyDetail.jsx
+++ src/pages/LobbyDetail/LobbyDetail.jsx
@@ -5,30 +5,31 @@ function LobbyDetail({}){
   return (
     <>
     <div className="main">
+      <div className="leftSide"> 
     <div className="gameName">  
       <h1>game name</h1>
     </div>
     <div>
       <h1>description</h1>
-      <h1> Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod  tempor 
-        incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim  veniam, quis nostrud
-      </h1>
+      <p> Lorem ipsum dolor sit amet, consectetur
+      </p>
     </div>
     </div>
+    <div className="rightside"> 
     <div>
       <h1>lobbyname</h1>
       <h1>lobbyy owner</h1>
       <h1>lobbyhead count</h1>
       <h1>remainf spot</h1>
     </div>
+    </div>
+    </div>
     <h1>non live message</h1>
     <form action="" method="POST" >
       <h1>.....</h1>
       <h1>Name</h1>
       <input type="text" />
-      <button>send</button>
+      <button type="button" className="btn btn-outline-success">Success</button>
     </form>
     
     </>
