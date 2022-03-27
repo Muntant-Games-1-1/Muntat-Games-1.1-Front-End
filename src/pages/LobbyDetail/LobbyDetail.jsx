5cb74cca9496108367ca37f3c23a7373d83aba7e
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-27 18:09:59 -0400

    lobby and namve done css

--- src/pages/LobbyDetail/LobbyDetail.jsx
+++ src/pages/LobbyDetail/LobbyDetail.jsx
@@ -4,35 +4,38 @@ import './LobbyDetail.css'
 function LobbyDetail({}){
   return (
     <>
-    <div className="lobby-detail">
-    <div className="main">
-      <div className="leftSide"> 
-    <div className="gameName">  
-      <h1>game name</h1>
-    </div>
-    <div>
-      <h1>description</h1>
-      <p> Lorem ipsum dolor sit amet, consectetur
-      </p>
-    </div>
-    </div>
-    <div className="rightside"> 
-    <div>
-      <h1>lobbyname</h1>
-      <h2>lobbyy owner</h2>
-      <h2>lobbyhead count</h2>
-      <h2>remainf spot</h2>
-    </div>
-    </div>
-    </div>
-    <h1>non live message</h1>
-    <form action="" method="POST" >
-      <h1>.....</h1>
-      <h1>Name</h1>
-      <input type="text" />
-      <button type="button" className="btn btn-outline-success">Success</button>
-    </form>
-    </div>
+      <div className="lobby-detail">
+        <div className="main">
+            <div className="leftSide"> 
+            <div className="leftSide-items"> 
+              <div className="gameName">  
+                <h1>game name</h1>
+              </div>
+              <div>
+                <h1>description</h1>
+                <p> Lorem ipsum dolor sit amet,</p>
+              </div>
+              </div>
+            </div>
+            <div className="rightside"> 
+              <div className="rightside-item">
+                <div className="lobbyname"> 
+                  <h1>lobbyname</h1>
+                </div>
+                <h2>lobbyy owner: XXX</h2>
+                <h2>lobbyhead count:XXX</h2>
+                <h2>remainf spot XXX</h2>
+              </div>
+            </div>
+        </div>
+        <h1>non live message</h1> 
+        <form action="" method="POST" >
+          <h1>.....</h1>
+          <h1>Name</h1>
+          <input type="text" />
+          <button type="button" className="btn btn-outline-success">Success</button>
+        </form>
+      </div>
     </>
   )
 }
