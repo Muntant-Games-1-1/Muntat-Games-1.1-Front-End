a5965281de572ffb9d9d447045a66c48008abc90
zendrias <zenas@live.com>
2022-03-27 17:24:48 -0400

    orginize code

--- src/pages/EditALobby/EditALobby.jsx
+++ src/pages/EditALobby/EditALobby.jsx
@@ -1,17 +1,14 @@
-
 import {useRef, useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom'
-// ! Change All The Input Values To Have The Pre-Edit Data
+
 function EditALobby({games, handleEditLobby}) {
+  // All State & Constants
   const location = useLocation()
-  // console.log(location.state)
-  // const previousGame =  games?.filter(gs =>{
-  //   return gs._id.toString() === location?.state?.game.toString()
-  // })
   const [validForm, setValidForm] =useState(true)
   const formElement = useRef()
   const [formData, setFormData] = useState(location.state)
 
+  // Event Handlers
 function handleSubmit(e){
 e.preventDefault()
 }
@@ -19,10 +16,12 @@ e.preventDefault()
 function handleChange(e){
   setFormData({...formData, [e.target.name] : e.target.value})
 }
-
+// Side Effects
 useEffect(() =>{
   formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
 }, [formData])
+
+// Jsx
   return (
     <div className='lobby-form'>
       <h1>Edit A Lobby</h1>
@@ -37,8 +36,8 @@ useEffect(() =>{
           value={formData.name}
         />
         <label htmlFor="chooseGame">Choose A Game</label>
-        <select id='chooseGame' onChange={handleChange} name='game' value={formData.chooseGame}>
-          {games && games.map((game, i) => {
+        <select id='chooseGame' onChange={handleChange} name='game'>
+          {games && games.map(game => {
               if(game._id.toString() === location.state.game.toString()){
                 return <option key={game._id} selected value={game._id}>{game.name}</option>
               } 
@@ -47,11 +46,12 @@ useEffect(() =>{
         </select>
         <label htmlFor="lobbyLimit">Player Limit</label>
         <input
-          type="text"
+          type="number"
           id='lobbyLimit'
           name='lobbyLimit'
           onChange={handleChange} 
           value={formData.lobbyLimit}
+          required
           />
         <button type='submit' disabled={!validForm}>Update</button>
       </form>
