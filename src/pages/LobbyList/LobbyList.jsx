6f251f389481f5d8c0d006e84903cb4b8f09afdc
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-28 11:10:32 -0400

    protective route completed

--- src/pages/LobbyList/LobbyList.jsx
+++ src/pages/LobbyList/LobbyList.jsx
@@ -2,20 +2,29 @@ import React  from "react";
 import {Link } from 'react-router-dom'
 // import LobbyDetail from './pages/LobbyDetail/LobbyDetail'
 const LobbyList = ({lobby, handleDeleteLobbies, user})=>{
+  console.log('lobby',lobby);
+  console.log('users',user);
   return(
     <>
       <h1> {lobby?.owner?.name}</h1>
       <h1>{lobby?.name}</h1>
+      {lobby.owner._id === user.profile?
+      <div> 
       <button onClick={()=> handleDeleteLobbies(lobby._id)}>
               Delete
       </button>
       < Link to="/edit-lobby" state={lobby}>
         Update Lobby
       </Link>
-      
+      </div>
+      :
+      <>
       <Link to="/lobby-detail" state={lobby} >
       <button>Join</button>
       </Link>
+      </>
+      }
+      
 
     </>
   )
