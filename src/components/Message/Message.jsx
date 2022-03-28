ee761a2b6178f865c57731e2dea5b7e360917965
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-03-28 11:43:07 -0400

    add styles to message box

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
+import styles from "./Message.module.css"
+
 
 export default function Message(props) {
   return (
-    <>
-      
-    </>
+    <div className={styles.msgContainer}>
+      <h1>Messages</h1>
+      {}
+      <p><span>{ props.owner }</span>{ props.content }</p>
+    </div>
   )
 }
