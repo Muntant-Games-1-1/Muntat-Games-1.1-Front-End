6cf916e54cd5532bbbe6f709b6ea2ea107a641c4
Bestnogm1 <koffiarielhessou@gmail.com>
2022-03-28 22:20:48 -0400

    fixed profile

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ const App = () => {
 					path="/lobby-detail"
 					element={
 						user ? (
-							<LobbyDetail />
+							<LobbyDetail  handleJoin={handleJoin}/>
 						) : (
 							<Navigate to="/login" />
 						)
--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
 
-const Profiles = ({ user,lobby}) => {
-  console.log('user',user);
-  console.log('wow', lobby.name);
+const Profiles = () => {
   const [profiles, setProfiles] = useState([])
-  console.log(profiles);
+
   useEffect(()=> {
     profileService.getAllProfiles()
     .then(profiles => setProfiles(profiles))
@@ -13,18 +11,18 @@ const Profiles = ({ user,lobby}) => {
 
   return (
     <>
-    <h1>{user?.name}</h1>
-    <h1>{user?.email}</h1>
-
       <h1>Hello. This is a list of all the profiles.</h1>
-      
+      {profiles.length ? 
         <>
           {profiles.map(profile=>
             <p key={profile._id}>{profile.name}</p>
           )}
         </>
+      :
+        <p>No profiles yet</p>
+      }
     </>
   )
 }
-
+ 
 export default Profiles
