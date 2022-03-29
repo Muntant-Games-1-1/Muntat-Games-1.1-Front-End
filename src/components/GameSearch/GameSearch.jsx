4ae4c4d4cbe6439d310391e91f32e5e28031a12f
zendrias <zenas@live.com>
2022-03-28 21:14:31 -0400

    finish formating in minimal styling

--- src/components/GameSearch/GameSearch.jsx
+++ src/components/GameSearch/GameSearch.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './GameSearch.module.css'
+import './GameSearch.css'
 function GameSearch({formData, handleChange, searchResults, handleGameSelection}) {
   return (
-<>
-<h3>Choose A Game</h3>
+    <>
+      <h3>Choose A Game</h3>
         <div
           className="search-container"
-          style={{display: 'flex', flexDirection: 'column'}}>
+        >
         < input
             id='chooseGame'
             type='search'
@@ -14,28 +14,25 @@ function GameSearch({formData, handleChange, searchResults, handleGameSelection}
             name='game'
             onChange={handleChange}
             required
-            style={{width: '20vw', minWidth: '300px'}}
           />
           <div
             className="dropDown"
-            style={{width: '20vw', minWidth: '300px', textAlign: 'center', maxHeight: '350px', overflow: 'auto'}}
-           >
-             {searchResults?.map(game => {
-               return(
+          >
+            {searchResults?.map(game => {
+              return(
                 <button
                   type='button'
                   className="searchResult"
                   key={game._id}
-                  style={{width: '100%'}}
                   onClick={handleGameSelection}
                 >
                 <p>{game.name}</p>
                 </button>
                 )
-             })}
+            })}
           </div>
-             </div>
-</>
+            </div>
+    </>
   );
 }
 
