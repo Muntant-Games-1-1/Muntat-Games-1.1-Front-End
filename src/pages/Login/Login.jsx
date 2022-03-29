6e59fc34a76e1239f488ea8b8aeb228975913da6
zendrias <zenas@live.com>
2022-03-28 21:54:15 -0400

    center div

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,12 +11,14 @@ const LoginPage = props => {
 
   return (
     <main className={styles.container}>
-      <h1>Log In</h1>
-      <p>{message}</p>
-      <LoginForm
-        handleSignupOrLogin={props.handleSignupOrLogin}
-        updateMessage={updateMessage}
-      />
+      <div className={styles.formBox}>
+        <h1>Log In</h1>
+        <p>{message}</p>
+        <LoginForm
+          handleSignupOrLogin={props.handleSignupOrLogin}
+          updateMessage={updateMessage}
+        />
+      </div>
     </main>
   )
 }
