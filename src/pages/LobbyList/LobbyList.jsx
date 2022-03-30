8535968187d2554965ffd1c41bffcd62e4bceb68
zendrias <zenas@live.com>
2022-03-30 13:31:22 -0400

    button colors

--- src/pages/LobbyList/LobbyList.jsx
+++ src/pages/LobbyList/LobbyList.jsx
@@ -10,21 +10,30 @@ const LobbyList = ({lobby, handleDeleteLobbies, user, handleJoin})=>{
 			{lobby.owner._id && lobby?.owner?._id === user?.profile ? (
 				<div className={styles.buttonContainer}>
 					<div className={styles.editDeleteContainer}>
-						<button onClick={() => handleDeleteLobbies(lobby._id)}>Delete</button>
-						<button>
+						<button
+							onClick={() => handleDeleteLobbies(lobby._id)}
+							className={styles.delete}>
+							Delete
+						</button>
+						<button className={styles.update}>
 						<Link to="/edit-lobby" state={lobby}>
 							Update Lobby
 						</Link>
 						</button>
 					</div>
 					<Link to={`/lobby-detail/${lobby._id}`} state={lobby}>
-						<button>View</button>
+						<button
+						className={styles.join}
+						>View</button>
 					</Link>
 				</div>
 			) : (
 				<div className={styles.buttonContainer}>
 					<Link to={`/lobby-detail/${lobby._id}`} state={lobby}>
-						<button onClick={() => handleJoin(lobby?._id)}>Join</button>
+						<button
+						 onClick={() => handleJoin(lobby?._id)}
+						 className={styles.join}
+						 >Join</button>
 					</Link>
 				</div>
 			)}
