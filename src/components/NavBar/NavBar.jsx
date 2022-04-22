4416defce647cdc75c24ccbf982f4f35b8b221f4
zendrias <zenas@live.com>
2022-04-22 19:16:40 -0400

    add links to front page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,32 @@
 import { Link } from 'react-router-dom'
 import styles from './NavBar.module.css'
 import logo from '../../images/logo.png'
-const NavBar = ({ user, handleLogout, handleGetAllLobby}) => {
-  return(
+const NavBar = ({ user, handleLogout, handleGetAllLobby }) => {
+  return (
     <>
-    {user ? (
+      {user ? (
         <nav>
-<Link onClick={handleGetAllLobby} to="/"><img src={logo} alt="logo" className={styles.logo}/></Link>
-<div className={styles.navLinks}>
-  <Link className={styles.link} to="/create-lobby">Create A Lobby</Link>
-  <Link className={styles.link} to="/add-game">Add A Game</Link>
-  <Link className={styles.logout} to="" onClick={handleLogout}>Logout</Link>
-</div>
-</nav>
+          <Link onClick={handleGetAllLobby} to="/"><img src={logo} alt="logo" className={styles.logo} /></Link>
+          <div className={styles.navLinks}>
+            <Link className={styles.link} to="/create-lobby">Create A Lobby</Link>
+            <Link className={styles.link} to="/add-game">Add A Game</Link>
+            <Link className={styles.logout} to="" onClick={handleLogout}>Logout</Link>
+          </div>
+        </nav>
       )
-      : 
-      <nav>
-        <Link onClick={handleGetAllLobby} to="/"><img src={logo} alt="logo" className={styles.logo}/></Link>
-        <div className={styles.navLinks}> 
-          <Link  className={styles.link} to="/login">Login</Link>
-          <Link  className={styles.link} to="/signup">Sign-up</Link>
-        </div>
-      </nav>
-      
-      
-      
+        :
+        <nav>
+          <Link onClick={handleGetAllLobby} to="/"><img src={logo} alt="logo" className={styles.logo} /></Link>
+          <div className={styles.navLinks}>
+            <Link className={styles.link} to="/login">Login</Link>
+            <Link className={styles.link} to="/signup">Sign-up</Link>
+          </div>
+        </nav>
+
+
+
       }
-      </>
+    </>
   )
 }
 
